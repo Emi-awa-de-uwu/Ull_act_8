3c53646b6cbc6022626d982664834f7909dda7da
Emi 33 <91576418+Emi-awa-de-uwu@users.noreply.github.com>
2021-11-03 22:36:56 -0700

    Add files via upload

---
+++ cliente.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>16 Emilio Guerrero Peralta 5-i</title>
+    
+	<link rel="stylesheet" href="estilos.css">
+</head>
+<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="https://e7.pngegg.com/pngimages/801/880/png-clipart-pizza-pizza-sticker-pepperoni-salami-pizza-food-orange.png">
+                 </a>
+                 <h6> Pizza and roll </h6>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="CLiente.php">Cliente</a></li>
+					<li><a href="pedido.php">pedido</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+<body>
+<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Membresia de la pizzeria</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+                <h4>¿Por que deberias sacar una de nuesdtras Membresia? </h4>
+                <br/>
+                    <p> Deberias poder sacar una de nuestras muy amigables y accecibles mebrecias ya que es un gran beneficio
+                    esta es una de las mejoires opciones que tenemos ya que dependiendo de la membrecia que saques puedes 
+                tener grandes y muy geniales descuentos ya si compras de nuestras membrecias mas altas podras recibir envios 
+            totalmente gratis y podras gozar de 2 pizzas gratis al mes </p>
+					<br/>
+                    <br/>
+                    <h4> ----------------------------------Membrecia plata-------------------------------------------- </h4>
+                    <br/>
+					<p> esta es la mas economica con un precio de $300 pesos mensuales
+                    con esta mebrecia tendar acceso a descuentos que van desde el 20% a el 60%
+                    ademas de eso tendras 2 viajes gratis al mes a cualquier parte de la ciudad
+                    y por ultimo tendras un ingrediente extra en 2 pizzas durante el mes </p>
+					<br/>
+                    <br/>
+                    <h4> -------------------------------------Membrecia oro------------------------------------------- </h4>
+                    <br/>
+					<p>esta membrecia tiene un costo de $450 pesos al mes y esta es la membrecia estandar
+                       con esta membrecia tendras el acceso a descuentos que van desde un
+                       20% a un 80% de descuento en algunas de las pizzas y no solo eso si no que 
+                       gozaras de 4 viajes gratis en el mes a cualquier parte de la ciudad
+                    y por ultimo tendras un ingrediente extra en 4 pizzas durante el mes
+                    </p>
+					<br/>
+                    <br/>
+                    <h4> ------------------------------Membrecia diamante------------------------------------------</h4>
+                    <br/>
+					<p> Esta membrecia tiene un costo de $600 pesos al mes y es la membrecia premium
+                        con este membrecia cuentas con 3 pizzas gratis al mes y descuentos del 20%
+                        al 80%y no solo eso si no que gozaras de 10 viajes gratis en el mes a cualquier
+                         parte de la ciudady por ultimo tendras un ingrediente extra en 8 pizzas 
+                         durante el mes y no solo eso si no que si visitas nuestras sucursales tendras un
+                         trato preferente por tu membrecia
+                    </p>
+				</article>
+			</div>
+<aside>
+                    <h5>Cliente</h5>
+                    <br>
+					<h3>Membresia de pizzas</h3>
+                    <h3>Ingrese aqui sus datos</h3>
+					 <br>
+                    <form action="welcome.php" method="post">
+                    nombre: <input type="text" name="nombre"><br>
+                    <br>
+                    ciudad: <input type="text" name="ciudad"><br>
+                    <br>
+                    Edad: <input type="text" name="edad"><br>
+                    <br>
+                    Telefono: <input type="text" name="Telefono"><br>
+                    <br>
+                    codigo postal: <input type="text" name="codigo postal"><br>
+                    <br>
+                    E-mail: <input type="text" name="email"><br>
+                    <br>
+                    tipo de pago: <input type="text" name="tipo de pago"><br>
+                    <input type="submit"> 
+                 </form>
+				</div>
+                </aside>
+                </div>
+	</section>
+    <footer>
+		<div class="wrapp">
+            <p> No lista 16 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/Emi-awa-de-uwu </p>
+		</div>
+	</footer>
+  </body>	
+</html>
